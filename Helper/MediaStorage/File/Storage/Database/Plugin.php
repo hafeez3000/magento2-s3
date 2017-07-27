8b68f2ceb8d35750a9150baf1b7cd36490e6b3a3
gemng <gem@4d.com.hk>
2017-07-27 15:09:14 +0800

    No need to save at this point since it is expected to be copied from tmp in the S3.renameFile function

--- Helper/MediaStorage/File/Storage/Database/Plugin.php
+++ Helper/MediaStorage/File/Storage/Database/Plugin.php
@@ -61,10 +61,6 @@ class Plugin
 
             
             $result = $subject->getStorageFileModel()->saveFile($file->getData(), true);
-            $s3result = $this->storageModel->saveFile($subject->getMediaRelativePath($filename));
-            if($s3result){
-                $result = false;
-            }
             return $result;
         }
         return $proceed($filename);
