b8887bfa96fb996b3bc4286a4fbfba85fff674b3
gemng <gem@4d.com.hk>
2017-08-01 19:05:57 +0800

    should not add / to the result. This will make other module has problem

--- Model/MediaStorage/File/Uploader/Plugin.php
+++ Model/MediaStorage/File/Uploader/Plugin.php
@@ -20,8 +20,7 @@ class Plugin
     public function afterSave($subject, $result)
     {
         if ($this->helper->checkS3Usage()) {
-            $result["file"] = "/".$result["file"];
-            $s3result = $this->storageModel->saveFile($this->storageHelper->getMediaRelativePath($result["path"].$result["file"]));
+            $s3result = $this->storageModel->saveFile($this->storageHelper->getMediaRelativePath($result["path"]."/".$result["file"]));
             if (!$s3result) {
                 $e = new Exception('Division by zero.', 0);
                 throw $e;
